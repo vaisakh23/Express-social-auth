063041bc3de40ef9a2bf9e7f00f2257e8cb5af57
vaisakh <vaisakhsindhu11@gmail.com>
2023-04-23 09:37:33 +0530

    env setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+require("dotenv").config();
 
 app = express();
 // routes
 app.use("/", indexRouter);
 app.use("/user", usersRouter);
 
-app.listen(3000, () => console.log('server started'))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`server started on port ${3000}`))
