b672ed29bfdfa5c2259fd272c4b2069a0d1887a6
vaisakh <vaisakhsindhu11@gmail.com>
2023-04-23 12:58:39 +0530

    hbs engine fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { engine } = require("express-handlebars");
+const { engine, create } = require("express-handlebars");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -8,17 +8,21 @@ require("dotenv").config();
 app = express();
 app.set("views", __dirname + "/views");
 
-// view engine
-app.set("view engine", ".hbs");
-app.engine(".hbs", engine({
+const hbs = create({
     layoutsDir: __dirname + "/views",
     defaultLayout: "layout",
     extname: ".hbs"
-}));
+});
+app.set("view engine", ".hbs");
+app.engine(".hbs", hbs.engine);
 
-// routes
 app.use("/", indexRouter);
 app.use("/user", usersRouter);
+
+// app.use("/test", async (req, res) => {
+//     const result = await hbs.renderView(req.app.get("views") + "/welcome.hbs")
+//     res.send(result)
+// })
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server started on port ${3000}`));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => res.render("welcome"))
+router.get("/", (req, res) => {
+    return res.render("welcome")
+});
 
 module.exports = router;
